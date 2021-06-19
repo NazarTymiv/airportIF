b2f99e9a10a9b4c31ed3247aa4b92dd8e85e0d7b
Nazar Tymiv <nazartumiv@gmail.com>
2021-06-19 14:41:36 +0300

    Fixed bug in tourism page with navigation

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
+    $servername = "localhost:3306/Airport";
+    $username = "airport";
+    $password = "Airport009";
+    $db = "airport";
 
-include_once("index.html");
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $db);
 
+    // Check connection
+    if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $result = $conn->query("SELECT * FROM airport.planes");
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+        echo "<h1 class='h5_b_1'>" . $row["id"] . $row["name"]."</h1>";
+        }
+    } else {
+        echo "0 results";
+    }
 ?>
